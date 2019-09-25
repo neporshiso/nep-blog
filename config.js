ef6907aadcd6e64a239570448d5d80289221ada7
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-09-24 22:02:06 -0400

    testing

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'Nep\'s Blog', // Required
+  author: 'Nep Orshiso', // Required
+  description: 'Studying Software Development at DigitalCrafts',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    // website: 'https://ryanfitzgerald.ca',
+    github: 'https://github.com/neporshiso',
+    twitter: 'https://twitter.com/NepOrshiso',
+    linkedin: 'https://www.linkedin.com/in/neporshiso/',
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
