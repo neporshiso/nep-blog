40f9ec2b67f42ae8d880f94c440d54827847b898
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-11-24 19:17:14 -0500

    minor updates

--- config.js
+++ config.js
@@ -7,10 +7,11 @@ module.exports = {
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
+    website: 'https://neporshiso.com',
     github: 'https://github.com/neporshiso',
     twitter: 'https://twitter.com/NepOrshiso',
     linkedin: 'https://www.linkedin.com/in/neporshiso/',
   },
   pathPrefix: '/',
-  siteUrl: 'https://neporshiso.com',
+  siteUrl: 'https://blog.neporshiso.com',
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-146861978-2`,
       },
     },
     `gatsby-plugin-feed`,
